fd3b5676e6f5d36566f3d0fa6758d81bbf84711a
Sara Fatima <sara.sagitarrius@gamil.com>
2023-05-02 14:22:22 +0500

    updated

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const connection = require('./config/database')
 const session = require('express-session')
 require('dotenv').config()
 const uri = process.env.MONGO_URI
 const port = process.env.PORT
 app.use(express.json())
+app.use(cors())
 const routes = require('./routes/router')
 const sessionKey = process.env.SESSION_SECRET
 const fileUpload = require('express-fileupload')
