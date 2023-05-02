45a637774959f93ed973a7b3135fb625d7f16f34
Sara Fatima <sara.sagitarrius@gamil.com>
2023-05-02 16:18:11 +0500

    updated

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const uri = process.env.MONGO_URI
 const port = process.env.PORT
 app.use(express.json())
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 const routes = require('./routes/router')
 const sessionKey = process.env.SESSION_SECRET
 const fileUpload = require('express-fileupload')
